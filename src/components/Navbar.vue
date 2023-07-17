66096f9515ef66b93bff67503ac78ce301d16be8
Emilio-PC <emiliosroche@gmail.com>
2023-07-16 21:56:14 -0600

    add nav bar and remove unused components/views

---
+++ src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+<template>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <RouterLink class="navbar-brand" to="/">Home</RouterLink>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            </ul>
+            <li class="nav-item d-flex">
+                <RouterLink class="nav-link" aria-current="page" to="/register">Register</RouterLink>
+            </li>
+            </div>
+        </div>
+    </nav>
+</template>
